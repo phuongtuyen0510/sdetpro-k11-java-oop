6ee4ab0ec21c93ce5715be378ae5fdc4ec0e4450
phuong.tuyen <phuongtuyen0510@gmail.com>
2022-09-11 23:36:29 +0700

    homework

--- src/lesson_02/Lab_2_odd_or_even_number.java
+++ src/lesson_02/Lab_2_odd_or_even_number.java
@@ -6,9 +6,9 @@ public class Lab_2_odd_or_even_number {
     public static void main(String[] args) {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Please input the number: ");
-            int yourNum = scanner.nextInt();
+            int userNum = scanner.nextInt();
 
-            if (yourNum / 2 == 0) {
+            if (userNum % 2 == 0) {
                 System.out.println("It's an even number");
             } else {
                 System.out.println("It's an odd number");
