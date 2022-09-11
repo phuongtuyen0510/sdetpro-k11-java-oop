b6ec3bbf99c6695807276e2357b681f0e7ed4824
phuong.tuyen <phuongtuyen0510@gmail.com>
2022-09-11 19:36:30 +0700

    Init

---
+++ src/lesson_01/VariableLearning.java
@@ -0,0 +1,19 @@
+package lesson_01;
+
+public class VariableLearning {
+    public static void main(String[] args) {
+        // snake_case | camelCase | PascalCase
+
+        // Declare = assign
+        int myNumber = 18; // camelCase
+        int my_num = 18; // snake_case
+        int MyNum = 18; // PascalCase
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        short myByteNum = 128;
+
+    }
+}
