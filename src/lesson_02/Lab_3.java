81b136d39832046a477a8b6f829809c695bbd98e
phuong.tuyen <phuongtuyen0510@gmail.com>
2022-09-11 23:32:27 +0700

    homework

--- src/lesson_02/Lab_3.java
+++ src/lesson_02/Lab_3.java
@@ -3,25 +3,24 @@ package lesson_02;
 import java.util.Scanner;
 
 public class Lab_3 {
-    public class Lab_1_calculateBMI {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Please enter your height (M): ");
-            float height = scanner.nextFloat();
-            System.out.println("Please enter your weight (kg): ");
-            float weight = scanner.nextFloat();
-            float BMI = weight / (height * 2);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your height (M): ");
+        float height = scanner.nextFloat();
+        System.out.println("Please enter your weight (kg): ");
+        float weight = scanner.nextFloat();
+        float BMI = weight / (height * 2);
 
-            if (BMI < 18.5) {
-                System.out.println("Underweight");
-            } else if (BMI <= 24.9) {
-                System.out.println("Normal weight");
-            } else if (BMI <= 29.9) {
-                System.out.println("Overweight");
-            } else {
-                System.out.println("Obesity");
-            }
-
+        if (BMI < 18.5) {
+            System.out.println("Underweight");
+        } else if (BMI <= 24.9) {
+            System.out.println("Normal weight");
+        } else if (BMI <= 29.9) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obesity");
         }
+
     }
+
 }
